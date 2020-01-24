4f778880c9c22103804e98738c62a189c83b6946
Tay-j <tay.j@unix500.hpc.wehi.edu.au>
2020-01-24 11:26:23 +1100

    Delete unused files

--- run.sh
+++
@@ -1,5 +0,0 @@
-cd /stornext/HPCScratch/lab_bahlo/MySQL_test 
-
-singularity instance start --bind ${HOME} --bind ${PWD}/mysql/var/lib/mysql/:/var/lib/mysql --bind ${PWD}/mysql/run/mysqld:/run/mysqld ./mysql.simg mysql 
-
-mysql -S ${PWD}/mysql/run/mysqld/mysqld.sock
