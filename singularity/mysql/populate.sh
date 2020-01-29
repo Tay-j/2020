0bb820d27b7e6fcc241913b322a57a9a46b3f8bc
John Tay <tay.j@unix401.wehi.edu.au>
2020-01-29 23:45:05 +0000

    Edited the database population method - i.e. through direct import of table

--- singularity/mysql/populate.sh
+++ singularity/mysql/populate.sh
@@ -14,12 +14,8 @@ mysql -S ${PWD}/mysql/run/mysqld/mysqld.sock -u $USER --password=password ${DATA
 
 mysql -S ${PWD}/mysql/run/mysqld/mysqld.sock -u $USER --password=password ${DATA} << EOF
 
-INSERT INTO \`geneticdata\` (\`marker\`,\`genotype\`,\`chr\`) VALUES (1,1,'chr1');
-
-INSERT INTO \`geneticmarkers\` (\`markerID\`,\`markername\`,\`chr\`,\`positionBP\`,\`REF\`,\`ALT\`,\`markertype\`) VALUES (1,'SNP1','chr1','1','A','T','SNP');
-
-INSERT INTO \`samples\` (\`sampleID\`,\`famID\`,\`individualID\`,\`fatherID\`,\`motherID\`,\`gender\`,\`affected\`,\`haplotype\`,\`ethnicity\`,\`mutation\`,\`validation\`,\`labID\`,\`datatype\`) VALUES (1,1,1,0,0,1,1,1,'y','RE1','pedigree','lab1','WGS'); 
-
-INSERT INTO \`pathogenicmutations\` (\`ID\`,\`gene\`,\`motif\`,\`disease\`,\`diseasemodel\`,\`positionBP\`,\`region\`,\`OMIMID\`) VALUES (1,'gene1','motif1','disease1','AD',1,'coding','OMIM1');
+USE ${DATA};
+LOAD DATA LOCAL INFILE '/stornext/Home/data/allstaff/t/tay.j/2020/1000genomes/samples.txt'
+INTO TABLE samples;
 
 EOF
